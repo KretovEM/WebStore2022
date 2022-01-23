6b7d62605ecc5ae2280ff7a3a8e13873b4bae10c
Кретов Евгений Михайлович <kretov.em@yandex.ru>
2022-01-23 11:57:38 +0300

    Регистрация сервисов бизнес-логики в проекте хоста WebAPI

--- Services/WebStore.WebAPI/Program.cs
+++ Services/WebStore.WebAPI/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Services;
+using WebStore.Services.Services.InSQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,54 @@ services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
+
+var database_type = builder.Configuration["Database"];
+switch (database_type)
+{
+    default: throw new InvalidOperationException($"Тип БД {database_type} не поддерживается");
+
+    case "SqlServer":
+        services.AddDbContext<WebStoreDB>(opt =>
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+        break;
+
+    case "Sqlite":
+        services.AddDbContext<WebStoreDB>(opt =>
+            opt.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"),
+                o => o.MigrationsAssembly("WebStore.DAL.Sqlite")));
+        break;
+}
+
+services.AddTransient<IDbInitializer, DbInitializer>();
+
+services.AddIdentity<User, Role>()
+   .AddEntityFrameworkStores<WebStoreDB>()
+   .AddDefaultTokenProviders();
+
+services.Configure<IdentityOptions>(opt =>
+{
+#if DEBUG
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequiredLength = 3;
+    opt.Password.RequiredUniqueChars = 3;
+#endif
+
+    opt.User.RequireUniqueEmail = false;
+    opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ1234567890";
+
+    opt.Lockout.AllowedForNewUsers = false;
+    opt.Lockout.MaxFailedAccessAttempts = 10;
+    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+});
+
+services.AddScoped<IEmployeesData, SqlEmployeesData>();
+services.AddScoped<IProductData, SqlProductData>();
+services.AddScoped<IOrderService, SqlOrderService>();
+
+
 
 var app = builder.Build();
 
